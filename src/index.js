a7ed2c4ab1709c74198e18110874e5e4faa66d72
Vy Nguyen <vy.nguyenduongthuy@gmail.com>
2023-07-21 12:48:43 +0700

    update file

--- src/index.js
+++ src/index.js
@@ -8,14 +8,22 @@ const port = 3005;
 
 app.use(morgan("combined"));
 
-app.engine("handlebars", engine());
-app.set("view engine ", "handlebars");
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine ", "hbs");
 app.set("views", path.join(__dirname, "/resources/views"));
 
 console.log("path:", __dirname + "/resource/views");
 
 app.get("/", (req, res) => {
   return res.render("home");
+});
+app.get("/news", (req, res) => {
+  return res.render("news");
 });
 
 // 127.0.0.1 - local host 3005
